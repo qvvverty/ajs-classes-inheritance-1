923390d4ef21898ce4819e9cf91905aa1f1945b7
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-09-26 12:18:55 +0300

    tests add

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,6 @@
-import './character/character';
+// export Bowman from './character/bowman';
+// export Swordsman from './character/swordsman';
+// export Magician from './character/magician';
+// export Undead from './character/undead';
+// export Zombie from './character/zombie';
+// export Daemon from './character/daemon';
